381bf94b176750200ee86ab59b22cdd5d3d12960
upaneeta <61232529+upaneeta@users.noreply.github.com>
2020-10-28 22:25:23 +0530

    Update imgtotext.py

--- imgtotext.py
+++ imgtotext.py
@@ -6,7 +6,7 @@ pt.pt.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
 def convert():
     img = Image.open('img.jpg')
-    text = pt.image_to_string(img)
-    print(text)
+    txt = pt.image_to_string(img)
+    print(txt)
 
 convert()    
